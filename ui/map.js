88b7d95fa11109af3b2f50ab620842e979504eaf
Jonni Lundy <jonathan.lundy@liferay.com>
2018-03-06 10:00:27 -0800

    Changes tabs to spaces

--- ui/map.js
+++ ui/map.js
@@ -2,22 +2,22 @@ var circle;
 var markers = [];
 
 function clearPlot() {
-	markers.forEach(function(marker) {
-		marker.setMap(null);
-	});
-	markers = [];
+  markers.forEach(function(marker) {
+    marker.setMap(null);
+  });
+  markers = [];
 }
 
 function plotMarkers(circle, name, geo_location, categories) {
-	var latLng = geo_location.split(', ');
-	var lat = latLng[0];
-	var lng = latLng[1];
+  var latLng = geo_location.split(', ');
+  var lat = latLng[0];
+  var lng = latLng[1];
 
-	var marker = new google.maps.Marker({
-		position: new google.maps.LatLng(lat, lng),
-		map: circle.map,
-		title: name
-	});
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(lat, lng),
+    map: circle.map,
+    title: name
+  });
 
   var markerMap = marker.map;
 
@@ -59,45 +59,45 @@ function createInfoWindow() {
 }
 
 function debounce(fn, delay) {
-	var id;
-	return function() {
-		var args = arguments;
-		clearTimeout(id);
-		id = setTimeout(function() {
-			fn.apply(this, args);
-		}, delay);
-	};
+  var id;
+  return function() {
+    var args = arguments;
+    clearTimeout(id);
+    id = setTimeout(function() {
+      fn.apply(this, args);
+    }, delay);
+  };
 }
 
 google.maps.event.addDomListener(window, 'load', function() {
-	var center = {
-		lat: 41.4993,
-		lng: -81.6944
-	};
+  var center = {
+    lat: 41.4993,
+    lng: -81.6944
+  };
 
-	var map = new google.maps.Map(document.getElementById('map-canvas'), {
-		center: center,
-		zoom: 14,
-		mapTypeControl: false,
-		fullscreenControl: false,
-		streetViewControl: false
-	});
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: center,
+    zoom: 14,
+    mapTypeControl: false,
+    fullscreenControl: false,
+    streetViewControl: false
+  });
 
-	circle = new google.maps.Circle({
-		fillColor: '#FF0000',
-		fillOpacity: 0.1,
-		map: map,
-		strokeColor: '#FF0000',
-		strokeOpacity: 0.5,
-		strokeWeight: 1.5,
-		center: center,
-		draggable: true,
-		editable: true,
-		radius: 1400
-	});
+  circle = new google.maps.Circle({
+    fillColor: '#FF0000',
+    fillOpacity: 0.1,
+    map: map,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.5,
+    strokeWeight: 1.5,
+    center: center,
+    draggable: true,
+    editable: true,
+    radius: 1400
+  });
 
-	var reloadVenuesDebounced = debounce(reloadVenues.bind(null, circle), 200);
-	google.maps.event.addListener(circle, 'radius_changed', reloadVenuesDebounced);
-	google.maps.event.addListener(circle, 'center_changed', reloadVenuesDebounced);
-	reloadVenues(circle);
+  var reloadVenuesDebounced = debounce(reloadVenues.bind(null, circle), 200);
+  google.maps.event.addListener(circle, 'radius_changed', reloadVenuesDebounced);
+  google.maps.event.addListener(circle, 'center_changed', reloadVenuesDebounced);
+  reloadVenues(circle);
 });
